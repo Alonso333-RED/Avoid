5401f743728bbfa3977d3528e0986d626b3f0a79
Alonso333-RED <alonsitoar018@gmail.com>
2026-04-01 00:17:15 -0300

    Builded the Level Selection System, the game is completely playable. For now just focusing on quality-of-life features.

--- UFO/levels/level_data.gd
+++ UFO/levels/level_data.gd
@@ -1,7 +1,8 @@
 extends Resource
 class_name LevelData
 
+@export var preview_path: String
 @export var name: String
 @export_multiline var description: String
-@export_enum("Muy Facil", "Facil", "Normal", "Difícil", "Muy Dificil") var difficulty: int
-@export var scene: PackedScene
+@export_enum("*", "**", "***", "****", "*****") var difficulty: int
+@export var scene: String
